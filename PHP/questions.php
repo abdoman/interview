42d04bb714fdc0198c69821dc96643faffab2eba
Tam Tran <ttran4040@gmail.com>
2015-05-17 12:38:01 -0500

    Updated for PHP

---
+++ PHP/questions.php
@@ -0,0 +1,50 @@
+<?php 
+/* Problem #1
+ *
+ * Create a function to parse through sample.csv and count and remove from the list any records that have duplicates first name and last name combination.
+ * The output should be saved in a file called results.php.
+ *
+ */
+
+// Write your solution to Problem #1 below this
+
+/* Problem #2
+ *
+ * Using previous file
+ * Write a php script that sends the results.php file to an external server. The servers credentials are stored in $_SERVER, $_USERNAME, $_PASSWORD.
+ */
+
+// Write your solution to Problem #2 below this
+
+
+/* Problem #3
+ *
+ * Given an array of numbers, create a function to echo mean, median and mode.
+ * sample array $test_sample = [1,2,10,22,32,123,2,55,32]
+ */
+
+// Write your solution to Problem #3 below this
+
+
+/* Problem #4
+ *
+ * Part 1. Design an abstract class Vehicle that gets the motor vehicle's name with a 'get_name' method.
+ * Part 2. Create a class Car that extends from Vehicle and returns the number of wheels with getNumWheels()
+ * Part 3. Repeat number two for class Bike
+ * Part 4. Write a method printNumWheels with parameter Vehicle that would echo the value returned based on their vehicle type
+ *
+ * Sample:
+ * $car = new Car();
+ * $bike = new Bike();
+ *
+ * printNumWheels($car);
+ * printNumWheels($bike);
+ *
+ * output:
+ * 'A Car has 4 wheels'
+ * 'A Bike has 2 wheels'
+ */
+
+// Write your solution to Problem #4 below this
+
+?>

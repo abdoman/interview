a929e472f0f27406bff1288063010218c8d96132
Tam Tran <ttran4040@gmail.com>
2015-05-17 11:44:10 -0500

    Updated questions

---
+++ Javascript/questions.js
@@ -0,0 +1,72 @@
+/* Problem #1
+ *
+ * Define a function, 'getTotal', that takes an arbitrary number of numeric arguments and returns
+ * the sum of those arguments.
+ */
+var getTotal;
+
+// Write your solution to Problem #1 below this
+
+
+
+/* Problem #2
+ *
+ * Write a function 'isTwoPassed' to tell whether 2 is passed as parameter or not?
+ *Example :
+ *   isTwoPassed(1,4)       // would return false
+ *   isTowPassed(5,3,1,2)   //would return true
+ */
+
+function isTwoPassed(){
+// Write your solution to Problem #2 below this
+}
+
+
+
+
+/* Problem #3
+ *
+ * Define a function, 'countTo', that accepts one argument, 'limit', that will be a Number. The
+ * function should print the numbers from 1 to 'limit'. For multiples of three print
+ * 'Fizz' instead of the number and for multiples of five print 'Buzz'. For numbers which are
+ * multiples of both three and five print 'FizzBuzz'.
+ * (Use 'document.write' to print the results.)
+ */
+var countTo;
+
+// Write your solution to Problem #3 below this
+
+
+
+/* Problem #4
+ * 
+ * Given an array of objects listed below, use javascript's console.log to output each property name and group_id by group from smallest to largest in the order they are ordered below.
+ IE: Group 3 will output 'Zyras Place', 'Argentinas Grove' in that order and so on for the remainder
+ */
+var properties = [
+    {group_id: 3, name: 'Zyras Place'},
+    {group_id: 4, name: 'Benson Place'},
+    {group_id: 3, name: 'Argentinas Grove'},
+    {group_id: 2, name: 'Plaza Hills'},
+    {group_id: 4, name: 'Broadway'},
+];
+
+
+// Write your solution to Problem #4 below this
+
+
+
+/* Problem #5
+ *
+ * For basic analytics on users screen sizes, create a function 'captureScreenSize' that would capture the current users screen size and sends an http post request to an external server.
+ * The servers IP address is stored in the variable '_SERVER'. 
+ */
+ 
+// Write your solution to Problem #5 below this
+function captureScreenSize() {
+
+};
+
+
+
+
---
+++ SQL/questions.sql
@@ -0,0 +1,18 @@
+/*
+The SQL questions are as follows:
+
+1. Create a table schema named runners with id and name
+Insert code for question 1 here.
+
+2. Create a table named races with the fields id, location_id, event and winner_id.
+Insert code for question 2 here.
+
+3. Write a querie to find the id and name of all racers that have not won an event
+Insert code for question 3 here.
+
+4. Write an efficient query to get the count of wins for id 33.
+Insert code for question 4 here.
+
+5. Given the following columns (name, address, home phone, cell phone, work phone, fax) from a csv, what are two ways you could structure the information into a table?
+Insert code for question 5 here.
+*/

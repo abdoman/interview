091edb0df5450e5a05cb15ccfafe29142008492c
Tam Tran <ttran4040@gmail.com>
2015-05-20 15:08:26 -0500

    Updated Python

---
+++ Python/questions.py
@@ -0,0 +1,34 @@
+def question_1():
+    """
+        Using the predefined list,"dates_list", sort the dates in chronological order. 
+        Your final solution should be a the original list of dates in order.
+    """
+    dates_list = ["Jun 1 2005  1:33PM , April 3 2015 5:00PM","Sunday 17 2012 10:00AM"]
+    dates_list_sorted = []
+    return dates_list_sorted
+
+def question_2():
+    """
+        Create a class with fields for first_name and last_name fields that 
+        can be used with the 'set' data type. 
+
+        Example: 
+        people = set()
+        john_doe = Person('john','doe')
+        people.add(john_doe)
+
+    """
+    class Person():
+        pass
+
+    people = set()    
+    return people
+
+def question_3():
+    """
+        Using multiple threads or processes, consolidate all files in the 'data' directory preserving new line characters.
+        The result should be a single list with all of the content from all files in the directory.
+    """
+    consolidated_files = []
+    #Do processing here
+    return consolidated_files
